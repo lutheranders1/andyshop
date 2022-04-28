66145447e8d94b58c90e7926a2b7434f7e5f5c5a
lutheranders1 <lutheranders@gmail.com>
2022-04-28 10:40:34 -0500

    aws update

--- backend/settings.py
+++ backend/settings.py
@@ -105,25 +105,25 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 #Database
 #https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'andyshop',
-#         'USER': 'postgres',
-#         'PASSWORD': 'lalacorp',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'andyshop',
+        'USER': 'postgres',
+        'PASSWORD': 'andyshop#1',
+        'HOST': 'andy-identifier.cwu7gtzasaxl.us-east-1.rds.amazonaws.com',
+        'PORT': '5432'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
