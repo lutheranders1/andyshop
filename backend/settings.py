b6c4136e1f84a0cd14871c4fa99123d9094ab32d
lutheranders1 <lutheranders@gmail.com>
2022-04-28 13:47:53 -0500

    aws update

--- backend/settings.py
+++ backend/settings.py
@@ -176,7 +176,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Dont forget to reset database connection and hide password
 AWS_QUERYSTRING_AUTH = False
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+AWS_DEFAULT_ACL = None
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
